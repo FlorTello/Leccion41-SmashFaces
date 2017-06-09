3864dfc5a8eb4570b6eccf375648440f0035a268
Flor Tello <flor.26071994@gmail.com>
2017-06-09 10:57:24 -0500

    Agrenado recursos

---
+++ js/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const state = {
+  stations: null,
+  selectedStation: null
+};
+
+$( _ => {
+
+  getJSON('stations.json', (err, json) => {
+
+    if (err) { return alert(err.message);}
+
+    state.stations = json;
+
+    const root = $('.root');
+    render(root);
+  });
+
+});
